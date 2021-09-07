3778a13bb3d7835d5771c0b55208df062f744b45
Codefident <piotr.klep01@wp.pl>
2021-09-07 17:15:48 +0200

    minor code improvements

--- src/system/Net.js
+++ src/system/Net.js
@@ -1,23 +1,23 @@
 class Net {
-    constructor() {
-        this.socket = null
-        this.connection()
-        this.createLobby = this.createLobby.bind(this)
-        this.joinLobby = this.joinLobby.bind(this)
-    }
+  constructor() {
+    this.socket = null;
+    this.connection();
+    this.createLobby = this.createLobby.bind(this);
+    this.joinLobby = this.joinLobby.bind(this);
+  }
 
-    connection() {
-        this.socket = io('http://localhost:3000/')
-        this.socket.on('connection')
-        console.log(this.socket)
-    }
+  connection() {
+    this.socket = io("http://localhost:3000/");
+    this.socket.on("connection");
+    console.log(this.socket);
+  }
 
-    createLobby(){
-        this.socket.emit("createLobby")
-    }
+  createLobby() {
+    this.socket.emit("createLobby");
+  }
 
-    joinLobby(){
-        const lobbyId = document.getElementById('join-lobby-id-input').value
-        this.socket.emit("joinLobby", lobbyId)
-    }
+  joinLobby() {
+    const lobbyId = document.getElementById("join-lobby-id-input").value;
+    this.socket.emit("joinLobby", lobbyId);
+  }
 }
